e8bc4147d4b86eebef2041d6699209b91ba9cb01
freddy barco <freddybarco@gmail.com>
2017-08-12 06:55:23 -0500

    version2

---
+++ app/src/main/java/app/com/freddybarco/miprimeraaplicacion/Main2Activity.java
@@ -0,0 +1,16 @@
+package app.com.freddybarco.miprimeraaplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Main2Activity extends AppCompatActivity {
+    private EditText in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        in = (EditText)findViewById(R.id.input);
+    }
+}
---
+++ app/src/main/java/app/com/freddybarco/miprimeraaplicacion/elementografico/grafico.java
@@ -0,0 +1,8 @@
+package app.com.freddybarco.miprimeraaplicacion.elementografico;
+
+/**
+ * Created by Freddy on 08/11/2017.
+ */
+
+public class grafico {
+}
